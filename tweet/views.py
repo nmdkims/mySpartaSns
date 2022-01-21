80499969afec16c787970eb4881867c38e58a0d0
KIMHOONHEE <nmdkims@gmail.com>
2022-01-21 23:43:37 +0900

    추가 - 로그인 기능구현 완료 Tweet저장 기능 구현

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TweetModel
 
 # Create your views here.
+
+def home(request):
+    user = request.user.is_authenticated
+    if user:
+        return redirect('/tweet')
+    else:
+        return redirect('/sign-in')
+
+
+def tweet(request):
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+
+        if user:
+            return render(request, 'tweet/home.html')
+        else:
+            return redirect('/sign-in')
+    elif request.method == 'POST':
+        user = request.user
+        my_tweet = TweetModel()
+        my_tweet.author = user
+        my_tweet.content = request.POST.get('my-content','')
+        my_tweet.save()
+        return redirect('/tweet')
--- user/views.py
+++ user/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
-
-
 # Create your views here.
+# user/views.py
+from django.shortcuts import render, redirect
+from .models import UserModel
+from django.contrib.auth import get_user_model  # 사용자가 데이터 베이스에 있는지 확인하는 함수
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
+
 def sign_up_view(request):
     if request.method == 'GET':
-        return render(request, 'user/signup.html')
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signup.html')
+
     elif request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
@@ -16,15 +27,14 @@ def sign_up_view(request):
         if password != password2:
             return render(request, 'user/signup.html')
         else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
 
-        return redirect('/sign-in')
+            exist_user = get_user_model().objects.filter(username=username)
 
-    return render(request, 'user/signup.html')
+            if exist_user:
+                return render(request, 'user/signup.html')  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
+            else:
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
+                return redirect('/sign-in')
 
 
 def sign_in_view(request):
@@ -32,14 +42,24 @@ def sign_in_view(request):
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
 
-        me = UserModel.objects.get(username=username)
-        if me.password == password:
-            request.session['user'] = me.username
-            return HttpResponse("로그인 성공!")
+        me = auth.authenticate(request, username=username, password=password)
+
+        if me is not None:
+            auth.login(request, me)
+            return redirect('/')
         else:
-            return redirect('/sign-in')
+            return redirect('/sin-in')
 
-        return HttpResponse('로그인 성성공!')
     elif request.method == 'GET':
-        return render(request, 'user/signin.html')
 
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
