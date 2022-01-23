184b219d666fb62ebe49ca6ec2b1dd184b3e332e
KIMHOONHEE <nmdkims@gmail.com>
2022-01-23 21:56:48 +0900

    추가 - 상세보기와 게시물 댓글 기능 구현

--- tweet/views.py
+++ tweet/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import TweetModel
+from .models import TweetComment
 from django.contrib.auth.decorators import login_required
 
 
@@ -35,3 +36,32 @@ def delete_tweet(request, id):
     my_tweet = TweetModel.objects.get(id=id)
     my_tweet.delete()
     return redirect('/tweet')
+
+@login_required
+def detail_tweet(request, id):
+    my_tweet = TweetModel.objects.get(id=id)
+    tweet_comment = TweetComment.objects.filter(tweet_id=id).order_by('-created_at')
+    return render(request,'tweet/tweet_detail.html',{'tweet':my_tweet,'comment':tweet_comment})
+
+
+@login_required
+def write_comment(request, id):
+    if request.method == 'POST':
+        comment = request.POST.get("comment","")
+        current_tweet = TweetModel.objects.get(id=id)
+
+        TC = TweetComment()
+        TC.comment = comment
+        TC.author = request.user
+        TC.tweet = current_tweet
+        TC.save()
+
+        return redirect('/tweet/'+str(id))
+
+
+@login_required
+def delete_comment(request, id):
+    comment = TweetComment.objects.get(id=id)
+    current_tweet = comment.tweet.id
+    comment.delete()
+    return redirect('/tweet/'+str(current_tweet))
